9e53cc7009a3dcc574fce00b8e20fb9aca44ab97
krystianluc <krystian.luc@q-perior.com>
2023-11-07 15:00:28 +0100

    spring boot overview

---
+++ 01-spring-boot-overview/06-properties-demo/src/main/java/com/lux2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,35 @@
+package com.lux2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String sayTeamInfo(){
+        return "Coach: " + coachName + " Team: " + teamName;
+    }
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 2k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day!";
+    }
+}
